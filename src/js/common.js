ead709429402b47fa47ceb6ded0d6916d430a097
ruslan <belozerzevruslan@gmail.com>
2019-02-05 15:06:25 +0200

    added

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,45 @@
 $(document).ready(function () {
+  headerNumbers();
 });
 $(window).resize(function () {
   innerWidth = $('body').innerWidth();
 });
+
+//global
+var innerWidth = $('body').innerWidth();
+
+function headerNumbers() {
+  var button = $('.header__numbers-button'),
+      block = $('.header__numbers-dropdown');
+
+  function stateCheck() {
+    block.each(function() {
+      if($(this).hasClass('header__numbers-dropdown_visible')) {
+        button.addClass('header__numbers-button_active');
+      } else {
+        button.removeClass('header__numbers-button_active');
+      }
+    });
+  }
+  function toggleHidden() {
+    if(innerWidth<769) {
+      block.removeClass('header__numbers-dropdown_visible');
+      stateCheck();
+    } else {
+      block.addClass('header__numbers-dropdown_visible');
+      stateCheck();
+    }
+  }
+
+  toggleHidden();
+  $(window).resize(function () {
+    toggleHidden();
+  });
+  stateCheck();
+
+  button.on('click', function() {
+    block.toggleClass('header__numbers-dropdown_visible');
+    stateCheck();
+  });
+}
 
